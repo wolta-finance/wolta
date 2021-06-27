4e91d1992aaedf901f694a7d4f315e41dc5cf3b3
George <george@abstractpoint.co.uk>
2021-06-27 13:15:08 +0100

    Some visual changes

--- packages/app/components/vaults-table.tsx
+++ packages/app/components/vaults-table.tsx
@@ -1,12 +1,9 @@
 import Image from 'next/image'
-import dynamic from 'next/dynamic'
-
+import Svg from './svg-patterns';
+import { encode } from "universal-base64";
 import EthTokenList from '../assets/blockchains/ethereum/tokenlist.json';
 const tokenSymbols = ['DAI', 'USDC', 'MKR', 'AAVE', 'COMP'];
 const vaults = EthTokenList.tokens.filter(each => tokenSymbols.includes(each.symbol));
-console.log(vaults);
-// const addr = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-// const DynamicComponent = dynamic(() => import(`../assets/blockchains/ethereum/assets/${addr}/logo.png`))
 
 const defaultProps = {
     vaults,
@@ -16,7 +13,14 @@ const Row = (props: any) => (
     <tr>
         <td className="border border-black px-2 py-2">
             <div className="flex">
-                <Image src={`/blockchains/ethereum/assets/${props.vault.address}/logo.png`} alt="DAI Logo" width={32} height={32} />
+                <div className="relative" style={{lineHeight: 0}}>
+                    <Image src={`/blockchains/ethereum/assets/${props.vault.address}/logo.png`} alt="DAI Logo" width={32} height={32} />
+                    <div className="inset-0 absolute" style={{
+                    backgroundImage: `url("data:image/svg+xml;base64,${encode(
+                        Svg({ color: 'white', density: 2, opacity: 0.7 })
+                    )}")`,
+                    }}></div>
+                </div>
                 <div className="flex items-center px-2">{props.vault.symbol}</div>
             </div>
             
@@ -29,10 +33,14 @@ const Row = (props: any) => (
 const VaultsTable = (props: any) => (
 <table className="table-auto w-full border-2 border-black">
   <thead>
-    <tr>
-      <th className="border border-black px-2 w-full">Vault</th>
-      <th className="border border-black px-2">Collective position</th>
-      <th className="border border-black px-2">Growth</th>
+    <tr style={{
+      backgroundImage: `url("data:image/svg+xml;base64,${encode(
+        Svg({ color: 'black', density: 2, opacity: 0.5 })
+      )}")`,
+    }}>
+      <th className="border border-black px-2 w-full"><span className="bg-white">Vault</span></th>
+      <th className="border border-black px-2"><span className="bg-white">Collective position</span></th>
+      <th className="border border-black px-2"><span className="bg-white">Growth</span></th>
     </tr>
   </thead>
   <tbody className="divide-x divide-black">
